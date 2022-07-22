95e217f69464e6439804ad14e2dd1294a91c556d
hanangul174 <108218331+hanangul174@users.noreply.github.com>
2022-07-22 17:28:49 +0500

    up

--- secondapp/src/componet/navbar.js
+++ secondapp/src/componet/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Home from './home'
 import "./nav.css"
 
 function Navbar() {
@@ -6,16 +7,16 @@ function Navbar() {
     <div className='navb'>
 <nav className="navbar navbar-expand-lg navbar-primary">
     <div className="container-fluid">
-        <a href="/Brand" className="navbar-brand">Brand</a>
+        <a href="/Brand" className="btn">Brand</a>
         <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse9">
             <span className="navbar-toggler-icon"></span>
         </button>
-        <div className='pages'>
+        <div className='buttons'>
         <div className="collapse navbar-collapse" id="navbarCollapse9">
             <div className="navbar-nav">
-                <a href="/" className="nav-item nav-link active">Home</a>
-                <a href="/about" className="nav-item nav-link">About</a>
-                <a href="/product" className="nav-item nav-link">Products</a>
+                <a href="/" className="btn">Home</a>
+                <a href="/about" className="btn">About</a>
+                <a href="/product" className="btn" >Products</a>
             </div>
            </div>
         </div>
