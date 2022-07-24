9dc0c2772145cd2c56588883d16f86af302cd65c
hanangul174 <108218331+hanangul174@users.noreply.github.com>
2022-07-24 16:06:29 +0500

    appliedjs

--- todolist/src/component/main/main.js
+++ todolist/src/component/main/main.js
@@ -1,6 +1,41 @@
 import React from 'react'
 import "./main.css"
 function Main() {
+    const[Todo,setTodo]=React.useState([]);
+    const[edit,setedit]=React.useState("");
+    const[idstore,setidstore]=React.useState(null);
+    const[show,setshow]=React.useState(false);
+    const [display,setdisplay]=React.useState("");
+    const handleonclick=()=>{
+        if (display===""){
+            alert("please enter task")
+        }
+        else{
+        const value=[...Todo,display];
+        setTodo(value);
+        setdisplay("");
+    }
+}
+    const handleondelete =(val)=> {
+        const del=Todo.filter((v)=>v !==val);
+        setTodo(del);
+    }
+    const handleonedit =(val,index) => {
+        setidstore(index)
+        setshow(!show);
+     if(edit){
+        const replic = Todo.map((e, i)=>{
+        if(i===index){
+            e = edit
+        }
+        return e;
+    });
+    setTodo(replic);
+    setedit('');
+       console.log('check', replic) 
+    } }
+
+
   return (
     <div className='container'>
         <div className='todo_container'>
@@ -12,19 +47,23 @@ function Main() {
                 <div className='bar'/>
             </div>
             <div className='input'>
-                <input className='input_field' type="text" />
-                <button className='button_design'>+</button>
+                <input className='input_field'value={display} onChange={(e)=>setdisplay(e.target.value)} type="text" />
+                
+
+                <button className='button_design' onClick={handleonclick}>+</button>
             </div>
             <div className='result'>
-                <div className='result_output'>
-                    <div className='result_content'>
-                        hanan
-                    </div>
+                {Todo.map((val,index)=>(
+                <div className='result_output' key={index}>
+                    <input className='result_content' disabled={show && idstore===index ?false :true}  value={show && idstore===index?edit :val} onChange={(e)=>setedit(e.target.value)}/>
+                 
                     <div className='buttons_action'>
-                        <button className='btn'>edit</button>
-                        <button className='btn'>delete</button>
+                        <button className='btn' onClick={()=>handleonedit(val,index)}>{show && idstore===index?"save":"edit"}</button>
+                    
+                        <button className='btn' onClick={()=>handleondelete(val)}>delete</button>
                     </div>
                 </div>
+                ))}
             </div>
         </div>
     </div>
